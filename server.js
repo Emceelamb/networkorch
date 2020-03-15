ce3e38740560fd19cca7442b7527ebada1623bd9
emceelamb <mlam@berkeley.edu>
2020-03-14 23:38:59 -0400

    add comments

--- server.js
+++ server.js
@@ -13,12 +13,19 @@ const noteList = ['E3', 'C4', 'F3', 'C6']
 app.use(express.static('public'));
 
 io.on('connection', (socket)=>{
+
+
   console.log('Connection was made!');
+
+  // Get IP Address of socket conn
   let address = socket.handshake.address;
   address=address.split(':')[3];
+
+
+  // Init Client obj
   let client = {
-    "address": address,
-    "socketId": socket.id,
+    "address"  : address,
+    "socketId" : socket.id,
   }
 
   let note = {
@@ -29,20 +36,18 @@ io.on('connection', (socket)=>{
 
   client["note"] = note;
 
+  // Add Client to list
   clientList.push(client);
-
   clientList = [...new Set(clientList)];
-  console.log(clientList)
-  io.emit('server response', `${clientList}`);
 
-  // io.sockets.socket(${socket.id}).emit(address)
+  io.emit('server response', `${clientList}`);
   console.log(`We have a client: ${socket.id}`);
 
+  // Send IP addr to client
   io.to(`${socket.id}`).emit('send ip', address)
 
-
+  // Send note to client
   io.to(`${socket.id}`).emit('play note', note)
-
   });
 
 io.on('disconnect', () =>{
@@ -71,16 +76,12 @@ server.listen(PORT, ()=>{
 var globalTimer = 0;
 function setLoop(timer){
   
-  console.log(globalTimer, timer % clientList.length );
-
+  //console.log(globalTimer, timer % clientList.length );
   let index = timer % clientList.length 
 
-  console.log(clientList[index].socketId, clientList[index].note)
-  globalTimer++;
-
-
+  //console.log(clientList[index].socketId, clientList[index].note)
   io.to(clientList[index].socketId).emit('play note',  clientList[index].note)
-
+  globalTimer++;
 }
 
 setInterval(function(){{setLoop(globalTimer)}}, 500)
